0007c0171ac462e09f770e5f5512522696ad58a7
BennoWu <648418123@outlook.com>
2021-01-27 11:00:55 +0800

    fix: import

--- src/constant.jsx
+++ src/constant.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 const name = 'ModalRouter: '
 export const NotMatched = `${name}In the map you provided, key didn't matched`
 export const InvalidElement_i = i => `${name}The Index:${i} Child isn't valid React Element`
